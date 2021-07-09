19865916fefc0dbc90a1344ac8b5967a7a79e72b
Jun Okabe <fofof200@gmail.com>
2021-07-10 00:51:34 +0900

    Fix build_local.sh

--- libs/build_local.sh
+++ libs/build_local.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # This script needs to be run on bash.
 
-#echo "building yasm"
-#pushd $(dirname $0)/yasm
-#./autogen.sh
-#./configure
-#make -j
-#popd
+echo "building yasm"
+pushd $(dirname $0)/yasm
+./autogen.sh
+./configure
+make -j
+popd
 
 echo "building mpir"
 pushd $(dirname $0)/mpir
