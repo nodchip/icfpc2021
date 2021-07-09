892bd3fa6ac521f2478c0ff2a57834bcaa95a4bc
nodchip <nodchip@gmail.com>
2021-07-09 23:24:18 +0900

    Added visualized problem image files.

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import matplotlib.pyplot as plt
+import os
+
 
 def get_x(point):
     return point[0]
@@ -10,7 +12,13 @@ def get_y(point):
     return point[1]
 
 
-def visualize(problem_file_path, pose_file_path):
+def visualize(problem_file_path, pose_file_path, output_file_path):
+    '''一つの問題を視覚化する。
+
+    problem_file_path [必須] 問題ファイルパス。
+    pose_file_path [任意] ポーズ (解答) ファイルパス。指定した場合、ポーズも描画する。
+    output_file_path [任意] 出力画像ファイルパス。指定しない場合、ウィンドウに出力する。指定した場合、画像ファイルに出力する。
+    '''
     with open(problem_file_path, 'r') as file:
         problem = json.load(file)
 
@@ -20,7 +28,11 @@ def visualize(problem_file_path, pose_file_path):
     max_x = max(problem['hole'] + problem['figure']['vertices'], key=get_x)[0]
     min_y = min(problem['hole'] + problem['figure']['vertices'], key=get_y)[1]
     max_y = max(problem['hole'] + problem['figure']['vertices'], key=get_y)[1]
-    plt.axes().set_ylim([max_y + 1, min_y - 1])
+    axes = plt.axes()
+    axes.set_ylim([max_y + 1, min_y - 1])
+
+    # 縦横比を1:1にする
+    axes.set_aspect('equal')
 
     # holeを描画する
     for index in range(0, len(problem['hole'])):
@@ -46,18 +58,37 @@ def visualize(problem_file_path, pose_file_path):
             dst = problem['figure']['vertices'][edge[1]]
             plt.plot(src, dst, color='blue')
 
-    plt.show()
+    if output_file_path:
+        plt.savefig(output_file_path)
+    else:
+        plt.show()
+
+    plt.close()
 
 
 def main():
     parser = argparse.ArgumentParser(description='Brain Wall Visualizer')
-    parser.add_argument('--problem', action='store', type=str,
-                        required=True, help='Problem file path.')
+    parser.add_argument('--problem', action='store',
+                        type=str, help='Problem file path. Visualize one file if specified.')
     parser.add_argument('--pose', action='store',
                         type=str, help='Pose file path.')
+    parser.add_argument('--problems', action='store', type=str,
+                        help='Problem folder path. Use to convert json files to image files.')
+    parser.add_argument('--output_image_directory_path', action='store', type=str,
+                        help='Problem folder path. Use to convert json files to image files.')
     args = parser.parse_args()
 
-    visualize(args.problem, args.pose)
+    if args.problem:
+        visualize(args.problem, args.pose, None)
+
+    if args.problems:
+        for problem_file_name in os.listdir(args.problems):
+            print(problem_file_name)
+            problem_file_path = os.path.join(args.problems, problem_file_name)
+            output_file_name = os.path.splitext(problem_file_name)[0] + ".png"
+            output_file_path = os.path.join(
+                args.output_image_directory_path, output_file_name)
+            visualize(problem_file_path, None, output_file_path)
 
 
 if __name__ == '__main__':
