6b29f6502f2a5f967dcceb108d1a2e032a3ca207
nodchip <nodchip@gmail.com>
2021-07-09 20:30:23 +0900

    Added /b option.

--- libs/build.bat
+++ libs/build.bat
@@ -4,8 +4,8 @@
 pushd %~dp0
 mkdir glog_vsbuild
 pushd glog_vsbuild
-cmake -DBUILD_TESTING:BOOL=OFF -DWITH_GFLAGS:BOOL=OFF ..\glog -G "Visual Studio 16 2019" || exit 1
-MSBuild glog.sln -target:glog -p:Configuration=Release;Platform=x64 || exit 1
-MSBuild glog.sln -target:glog -p:Configuration=Debug;Platform=x64 || exit 1
+cmake -DBUILD_TESTING:BOOL=OFF -DWITH_GFLAGS:BOOL=OFF ..\glog -G "Visual Studio 16 2019" || exit /b 1
+MSBuild glog.sln -target:glog -p:Configuration=Release;Platform=x64 || exit /b 1
+MSBuild glog.sln -target:glog -p:Configuration=Debug;Platform=x64 || exit /b 1
 popd
 popd
