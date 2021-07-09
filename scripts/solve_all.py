0e9a2cf7e1755b06469e0cecb385419660ac32c8
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2021-07-10 04:33:11 +0900

    separate common paths to common.py

--- scripts/solve_all.py
+++ scripts/solve_all.py
@@ -9,14 +9,7 @@ import os
 import tqdm
 import pandas as pd
 
-from pathlib import Path
-REPO_DIR = Path(__file__).resolve().parent.parent.resolve()
-DEFAULT_SOLUTIONS_DIR = REPO_DIR / 'solutions'
-DEFAULT_PROBLEMS_DIR = REPO_DIR / 'data' / 'problems'
-if sys.platform == 'win32':
-    EXE_DIR = REPO_DIR / 'vs' / 'x64' / 'Release'
-else:
-    EXE_DIR = REPO_DIR / 'src'
+from common import *
 
 def main():
     parser = argparse.ArgumentParser(description='Brain Wall batch solver')
