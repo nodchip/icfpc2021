571212a38a60b9abb651421a9cb2148dab4ed2e6
Jun Okabe <fofof200@gmail.com>
2021-07-10 15:22:59 +0900

    Change solve_all use multiprocessing. Default parallelism is num_cpu. Fix solve_all.py improvement place.

--- scripts/solve_all.py
+++ scripts/solve_all.py
@@ -8,8 +8,99 @@ import glob
 import os
 import tqdm
 import pandas as pd
+import multiprocessing as mp
+from multiprocessing import Pool
 
 from common import *
+
+def job(params):
+    problem_json, args = params
+    name = Path(problem_json).name
+    print('\n' + name)
+    mo = re.search(r'^(\d+)', name)
+    if mo:
+        num = int(mo.groups()[0])
+        out_path = args.output_dir / f'{num}.pose.json'
+    else:
+        out_path = args.output_dir / (Path(problem_json).stem + '.pose.json')
+
+    # solve.
+    tmp_out_path = out_path.with_suffix(out_path.suffix + '.tmp')
+    subprocess.run([str(EXE_DIR / 'solver'), 'solve', args.solver_name, str(Path(problem_json).resolve()), str(tmp_out_path)], cwd=EXE_DIR)
+    with open(tmp_out_path, 'r') as fi:
+        j = json.load(fi)
+    new_is_valid = j['meta']['judge']['is_valid']
+    new_dislikes = j['meta']['judge']['dislikes']
+
+    row = {'name': name, 'new_is_valid': new_is_valid, 'new_dislikes': new_dislikes}
+
+    if not new_is_valid:
+        print(name, 'skip (invalid)')
+        os.unlink(tmp_out_path)
+        return dict(row, result='invalid')
+
+    action = 'force'
+    exists = out_path.is_file()
+    if args.overwrite_option == 'never':
+        if exists:
+            action = 'skip'
+        else:
+            action = 'force'
+    elif args.overwrite_option == 'force':
+        action = 'force'
+    elif args.overwrite_option == 'improvement': 
+        if exists:
+            action = 'compare'
+        else:
+            action = 'force'
+        
+    if action == 'skip':
+        os.unlink(tmp_out_path)
+        print(name, 'skip (never overwrite)')
+        return dict(row, result='skip_never_overwrite')
+    elif action == 'force':
+        shutil.move(tmp_out_path, out_path)
+        print(name, 'wrote')
+        return dict(row, result='wrote')
+    
+    elif action == 'compare':
+        with open(out_path, 'r') as fi:
+            j = json.load(fi)
+
+        has_judge = 'meta' in j and 'judge' in j['meta']
+        if not has_judge:
+            tmp2_out_path = out_path.with_suffix(out_path.suffix + '.tmp2')
+            shutil.copyfile(out_path, tmp2_out_path)
+            subprocess.run([str(EXE_DIR / 'judge'), str(Path(problem_json).resolve()), str(tmp2_out_path)], cwd=EXE_DIR)
+            with open(tmp2_out_path, 'r') as fi:
+                j = json.load(fi)
+            os.unlink(tmp2_out_path)
+        old_is_valid = j['meta']['judge']['is_valid']
+        old_dislikes = j['meta']['judge']['dislikes']
+        row['old_is_valid'] = old_is_valid
+        row['old_dislikes'] = old_dislikes
+       
+        improved = False
+        if old_is_valid:
+            if new_is_valid:
+                improved = new_dislikes < old_dislikes
+            else:
+                pass
+        else:
+            if new_is_valid:
+                improved = True
+            else:
+                improved = new_dislikes < old_dislikes # both invalid but a better score.
+        
+        if improved:
+            shutil.move(tmp_out_path, out_path)
+            print(name, 'wrote (improved)')
+            return dict(row, result='wrote_improve')
+        else:
+            os.unlink(tmp_out_path)
+            print(name, 'skip (did not improve)')
+            return dict(row, result='skip_noimprove')
+ 
 
 def main():
     parser = argparse.ArgumentParser(description='Brain Wall batch solver')
@@ -17,100 +108,22 @@ def main():
     parser.add_argument('overwrite_option', default='improvement', choices=['force', 'improvement', 'never'], help='when to overwite existing files')
     parser.add_argument('--output-dir', default=DEFAULT_SOLUTIONS_DIR, type=Path)
     parser.add_argument('--problems-dir', default=DEFAULT_PROBLEMS_DIR, type=Path)
+    parser.add_argument('-j', '--parallel', default=mp.cpu_count())
     args = parser.parse_args()
 
     args.problems_dir = args.problems_dir.resolve()
     args.output_dir = args.output_dir.resolve()
     args.output_dir.mkdir(exist_ok=True)
-        
+      
     rows = []
-    for problem_json in tqdm.tqdm(glob.glob(str(args.problems_dir / '*.json'))):
-        name = Path(problem_json).name
-        print('\n' + name)
-        mo = re.search(r'^(\d+)', name)
-        if mo:
-            num = int(mo.groups()[0])
-            out_path = args.output_dir / f'{num}.pose.json'
-        else:
-            out_path = args.output_dir / (Path(problem_json).stem + '.pose.json')
 
-        # solve.
-        tmp_out_path = out_path.with_suffix(out_path.suffix + '.tmp')
-        subprocess.run([str(EXE_DIR / 'solver'), 'solve', args.solver_name, str(Path(problem_json).resolve()), str(tmp_out_path)], cwd=EXE_DIR)
-        with open(tmp_out_path, 'r') as fi:
-            j = json.load(fi)
-        new_is_valid = j['meta']['judge']['is_valid']
-        new_dislikes = j['meta']['judge']['dislikes']
+    pool = Pool(processes=args.parallel)
+    jobs = [(json, args) for json in glob.glob(str(args.problems_dir / '*.json'))]
 
-        row = {'name': name, 'new_is_valid': new_is_valid, 'new_dislikes': new_dislikes}
-
-        if not new_is_valid:
-            print(name, 'skip (invalid)')
-            rows.append(dict(row, result='invalid'))
-            os.unlink(tmp_out_path)
-            continue
-
-        action = 'force'
-        exists = out_path.is_file()
-        if args.overwrite_option == 'never':
-            if exists:
-                action = 'skip'
-            else:
-                action = 'force'
-        elif args.overwrite_option == 'force':
-            action = 'force'
-        elif args.overwrite_option == 'improvement': 
-            if exists:
-                action = 'compare'
-            else:
-                action = 'force'
-        
-        if action == 'skip':
-            os.unlink(tmp_out_path)
-            print(name, 'skip (never overwrite)')
-            rows.append(dict(row, result='skip_never_overwrite'))
-        elif action == 'force':
-            shutil.move(tmp_out_path, out_path)
-            print(name, 'wrote')
-            rows.append(dict(row, result='wrote'))
-        
-        elif action == 'compare':
-            with open(out_path, 'r') as fi:
-                j = json.load(fi)
-
-            has_judge = 'meta' in j and 'judge' in j['meta']
-            if not has_judge:
-                tmp2_out_path = out_path.with_suffix(out_path.suffix + '.tmp2')
-                shutil.copyfile(out_path, tmp2_out_path)
-                subprocess.run([str(EXE_DIR / 'judge'), str(Path(problem_json).resolve()), str(tmp2_out_path)], cwd=EXE_DIR)
-                with open(tmp2_out_path, 'r') as fi:
-                    j = json.load(fi)
-                os.unlink(tmp2_out_path)
-            old_is_valid = j['meta']['judge']['is_valid']
-            old_dislikes = j['meta']['judge']['dislikes']
-            rows['old_is_valid'] = old_is_valid
-            rows['old_dislikes'] = old_dislikes
-            
-            improved = False
-            if old_is_valid:
-                if new_is_valid:
-                    improved = new_dislikes < old_dislikes
-                else:
-                    pass
-            else:
-                if new_is_valid:
-                    improved = True
-                else:
-                    improved = new_dislikes < old_dislikes # both invalid but a better score.
-            
-            if improved:
-                shutil.move(tmp_out_path, out_path)
-                print(name, 'wrote (improved)')
-                rows.append(dict(row, result='wrote_improve'))
-            else:
-                os.unlink(tmp_out_path)
-                print(name, 'skip (did not improve)')
-                rows.append(dict(row, result='skip_noimprove'))
+    with tqdm.tqdm(total=len(jobs)) as t:
+        for result in pool.imap_unordered(job, jobs, chunksize=1):
+            t.update(1)
+            rows.append(result)
 
     df = pd.DataFrame(rows)
     df.to_csv('solve_all.csv', index=False)
