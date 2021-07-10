49ae61b978eddf2405d5a6528c523ca4f81f7070
Hitoshi Yoshida <github@peria.jp>
2021-07-10 10:53:57 +0900

    Start web interface

--- www/entrypoint.py
+++ www/entrypoint.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#!/usr/bin/python3
+
+import flask
+import os
+import re
+
+app = flask.Flask(__name__)
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROBLEMS_DIR = os.path.join(ROOT_DIR, 'data', 'problems')
 
 
 @app.route('/')
 def home():
-    return render_template('home.html', title='ICFPC2020 sanma team home page')
+    return flask.render_template('home.html', title='ICFPC2021 sanma')
+
+
+@app.route('/problems.html')
+def show_problems():
+    ids = sorted([int(re.sub(r'\D', '', x)) for x in os.listdir(path=PROBLEMS_DIR)])
+
+    problems = [{
+        'id': id,
+        'image': 'images/{}.problem.png'.format(id),
+        'file': os.path.join(PROBLEMS_DIR, '{}.problem.json'.format(id)),
+    } for id in ids]
+    context = {
+        'problems': problems,
+    }
+    return flask.render_template('problems.html', title='Problems', **context)
+
+@app.route('/images/<path:path>')
+def send_images(path):
+    return flask.send_from_directory('./images', path)
 
 
 if __name__ == "__main__":
