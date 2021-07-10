9071747be3872f287335344b4337792d33ac6b08
Jun Okabe <fofof200@gmail.com>
2021-07-11 03:55:20 +0900

    Add script for making dependency graph of bonuses.

---
+++ scripts/dependency.py
@@ -0,0 +1,20 @@
+import json
+import os
+import glob
+from graphviz import Digraph
+
+def main():
+    dg = Digraph(format='png')
+    for file in glob.glob('../data/problems/*.json'):
+        filename = os.path.basename(file)
+        problem = filename[0:filename.find('.')]
+        with open(file) as f:
+            data = json.load(f)
+            for bonus in data['bonuses']:
+                tp = f"{bonus['problem']}"
+                tb = f"{bonus['bonus']}"
+                dg.node(tp, style="filled", fillcolor=("red" if tb == "GLOBALIST" else "blue"))
+                dg.edge(problem, tp)
+    dg.render('./dgraph', view=True)
+
+main()
