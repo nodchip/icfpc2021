685a54ba7da449e3a90786e4cfa7db9568a112c2
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2021-07-11 19:45:29 +0900

    find FooSolver by "Foo"

--- src/solver_registry.cpp
+++ src/solver_registry.cpp
@@ -1,6 +1,23 @@
 #include "stdafx.h"
 #include "solver_registry.h"
 #include <iostream>
+#include <fmt/format.h>
+
+SolverBase::Ptr SolverRegistry::getSolver(std::string name) {
+  auto reg = getRegistry();
+  {
+    auto it = reg.find(name);
+    if (it != reg.end()) return it->second.factory();
+  }
+  {
+    auto it = reg.find(name + "Solver");
+    if (it != reg.end()) {
+      LOG(INFO) << fmt::format("requested [{}] -> found [{}Solver]", name, name);
+      return it->second.factory();
+    }
+  }
+  return {};
+}
 
 void SolverRegistry::displaySolvers() {
   for (auto it = getRegistry().begin(); it != getRegistry().end(); ++it) {
