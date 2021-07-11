f1cd8e0d08ffc8be0b41e82658405ee8536fdfc6
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2021-07-12 01:25:27 +0900

    add NaiveSearchSolver

---
+++ src/solvers/naive_search_solver.cpp
@@ -0,0 +1,219 @@
+#include "stdafx.h"
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <fmt/format.h>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include "contest_types.h"
+#include "solver_registry.h"
+#include "visual_editor.h"
+#include "judge.h"
+
+namespace NNaiveSearchSolver {
+namespace bg = boost::geometry;
+using BoostPoint = bg::model::d2::point_xy<double>;
+using BoostPolygon = bg::model::polygon<BoostPoint>;
+using BoostLinestring = bg::model::linestring<BoostPoint>;
+
+template <typename T>
+void shrink(T& point_a, T& point_b) {
+  constexpr double eps = 1e-6;
+  auto shrink_a = point_a * (1.0 - eps) + point_b * eps;
+  auto shrink_b = point_b * (1.0 - eps) + point_a * eps;
+  point_a = shrink_a;
+  point_b = shrink_b;
+}
+
+template <typename T>
+BoostPoint ToBoostPoint(const T& point) {
+  const auto [x, y] = point;
+  return BoostPoint(x, y);
+}
+
+template <typename T>
+BoostPolygon ToBoostPolygon(const std::vector<T>& points) {
+  BoostPolygon polygon;
+  for (std::size_t i = 0; i <= points.size(); ++i) {
+    polygon.outer().push_back(ToBoostPoint(points[i % points.size()]));
+  }
+  if (bg::area(polygon) < 0.0) {
+    bg::reverse(polygon);
+  }
+  return polygon;
+}
+
+std::vector<Point> enumerate_interior_points(const SProblem& problem) {
+  integer ymin = INT_MAX, ymax = INT_MIN;
+  integer xmin = INT_MAX, xmax = INT_MIN;
+  for (auto p : problem.hole_polygon) {
+    xmin = std::min(xmin, get_x(p));
+    ymin = std::min(ymin, get_y(p));
+    xmax = std::max(xmax, get_x(p));
+    ymax = std::max(ymax, get_y(p));
+  }
+
+  auto bg_hole_polygon = ToBoostPolygon(problem.hole_polygon);
+  std::vector<Point> points;
+  for (int y = ymin; y <= ymax; ++y) {
+    for (int x = xmin; x <= xmax; ++x) {
+      if (bg::within(ToBoostPoint(Point{x, y}), bg_hole_polygon)) {
+        points.emplace_back(x, y);
+      }
+    }
+  }
+  LOG(INFO) << fmt::format("found {} interior points", points.size());
+  return points;
+}
+
+class NaiveSearchSolver : public SolverBase {
+public:
+  NaiveSearchSolver() { }
+  SolverOutputs solve(const SolverArguments &args) override {
+    SolverOutputs ret;
+
+    // init.
+    ret.solution = args.optional_initial_solution ? args.optional_initial_solution : args.problem->create_solution();
+
+    // develop.
+    SVisualEditorPtr editor;
+    if (args.visualize) {
+      editor = std::make_shared<SVisualEditor>(args.problem, "NaiveSearchSolver", "visualize");
+    }
+
+    // prepare.
+    const auto interior_points = enumerate_interior_points(*args.problem);
+    const std::vector<std::vector<int>> edges_from_vertex_cache = edges_from_vertex(*args.problem);
+    const int V = args.problem->vertices.size();
+
+    auto bg_hole_polygon = ToBoostPolygon(args.problem->hole_polygon);
+    auto is_good_edge = [&](const Point& p, const Point& q) {
+      BoostLinestring linestring{ToBoostPoint(p), ToBoostPoint(q)};
+      std::vector<BoostLinestring> differences;
+      bg::difference(linestring, bg_hole_polygon, differences);
+      return differences.empty();
+    };
+
+    // (starting position, radius**2) -> (available points)
+    using SKey = std::pair<Point, int>;
+    std::map<SKey, std::vector<Point>> movable_cache;
+    // exact lookup.
+    auto get_movable_points = [&](Point p, int d2) -> std::vector<Point> {
+      const SKey query {p, d2};
+      auto it = movable_cache.find(query);
+      if (it != movable_cache.end()) {
+        return it->second;
+      }
+      std::vector<Point> points;
+      for (Point ip : interior_points) {
+        if (d2 == distance2(p, ip) && is_good_edge(p, ip)) {
+          points.push_back(ip);
+        }
+      }
+      movable_cache.insert(it, {query, points});
+      return points;
+    };
+    // consider tolerance.
+    auto get_movable_points_with_tolerance = [&](Point p, int original_distance2) -> std::vector<Point> {
+      // |d_new/d_old - 1| <= eps / 1000000
+      constexpr int k = 1'000'000;
+      const int e = args.problem->epsilon;
+      const int distance2_min = std::floor(original_distance2 * double(k - e) / double(k));
+      const int distance2_max = std::ceil(original_distance2 * double(k + e) / double(k));
+      std::vector<Point> all_points;
+      for (int d2 = distance2_min; d2 <= distance2_max; ++d2) {
+        if (std::abs(d2 - original_distance2) * k <= original_distance2 * e) { // to make sure.
+          auto points = get_movable_points(p, d2);
+          std::copy(points.begin(), points.end(), std::back_inserter(all_points));
+        }
+      }
+      return all_points;
+    };
+
+    constexpr int USED = 1;
+    constexpr int REMAINING = 0;
+    struct State {
+      // placing vertices[vid] to position p.
+      int depth = 0;
+      int vid = 0;
+      Point p = {0, 0};
+      // remaining indices after placing this node.
+      std::vector<int> indices_flag;
+
+      State(int depth, int vid, Point p, const std::vector<int>& indices_flag)
+        : depth(depth), vid(vid), p(p), indices_flag(indices_flag) {}
+    };
+    using StatePtr = std::shared_ptr<State>;
+    auto create_fixed_indices = [&](StatePtr s) {
+      std::vector<int> fixed_indices;
+      for (int i = 0; i < V; ++i) {
+        if (s->indices_flag[i] == USED) {
+          fixed_indices.push_back(i);
+        }
+      }
+      return fixed_indices;
+    };
+
+    std::stack<StatePtr> stack;
+
+    // start from any valid points.
+    {
+      const int start_index = 0;
+      std::vector<int> indices_flag(V, REMAINING);
+      indices_flag[start_index] = USED;
+      for (auto ip : interior_points) {
+        stack.push(std::make_shared<State>(1 /* depth */, start_index, ip, indices_flag));
+      }
+    }
+
+    int64_t counter = 0;
+    int max_depth = 1;
+    while (!stack.empty()) {
+      StatePtr s = stack.top(); stack.pop();
+      const auto p_bak = ret.solution->vertices[s->vid];
+      ret.solution->vertices[s->vid] = s->p;
+      {
+        ++counter;
+        max_depth = std::max(max_depth, s->depth);
+
+        if ((counter + 1) % 1 == 0 && editor) {
+          editor->set_persistent_custom_stat(fmt::format("visited {}, max depth {}", counter, max_depth));
+          editor->set_marked_indices(create_fixed_indices(s));
+          editor->set_pose(ret.solution);
+          editor->show(1);
+        }
+      }
+
+      for (int eid : edges_from_vertex_cache[s->vid]) {
+        auto [u, v] = args.problem->edges[eid];
+        auto counter_vid = u == s->vid ? v : u;
+
+        if (s->indices_flag[counter_vid] == REMAINING) {
+          // push nodes for this unused edge (s->vid, counter_vid)
+          auto original_distance2 = distance2(args.problem->vertices[s->vid], args.problem->vertices[counter_vid]);
+          auto movable_points = get_movable_points_with_tolerance(s->p, original_distance2);
+
+          LOG(INFO) << fmt::format("pivot vid={} next {} candidates", s->vid, movable_points.size());
+
+          for (auto p : movable_points) {
+            auto new_remaining_index_flag = s->indices_flag;
+            new_remaining_index_flag[counter_vid] = USED;
+            stack.push(std::make_shared<State>(s->depth + 1, counter_vid, p, new_remaining_index_flag));
+          }
+        }
+      }
+      ret.solution->vertices[s->vid] = p_bak;
+    }
+    LOG(INFO) << fmt::format("total nodes = {}, max depth = {}", counter, max_depth);
+
+    return ret;
+  }
+
+};
+}
+
+REGISTER_SOLVER("NaiveSearchSolver", NNaiveSearchSolver::NaiveSearchSolver);
+// vim:ts=2 sw=2 sts=2 et ci
+
