0beaf7544cf08c196445b83e79daa515ae68b228
Hisahiro Ikari <hisahiro.ikari@zio.co.jp>
2021-07-12 13:30:24 +0900

    SimpeMatchFullResearchの実装

--- src/solvers/full_research.cpp
+++ src/solvers/full_research.cpp
@@ -4,6 +4,7 @@
 #include "timer.h"
 #include "solver_registry.h"
 
+#include <boost/geometry.hpp>
 #include <queue>
 #include <set>
 #include <fmt/core.h>
@@ -407,7 +408,7 @@ SSolutionPtr FitUnstrictedPoints(SProblemPtr problem, SSolutionPtr solution, con
   return Fit_Unstricted_Points(problem, solution, decided_points, create_vertices_distances(problem), hash_map_decided, counter);
 }
 
-SSolutionPtr dfs_holes(SProblemPtr problem, SSolutionPtr solution, HashMap& hash_map_decided, const mat& hole_distances, const mat& vertices_distances, const std::vector<std::vector<double>>& upperlimit_distances, std::vector<int>& v, int V, int H, Timer& timer, std::vector<int>& used_vertices, const vec_holenum& v_h, long long& counter) {
+SSolutionPtr dfs_holes(SProblemPtr problem, SSolutionPtr solution, HashMap& hash_map_decided, const mat& hole_distances, const mat& vertices_distances, const std::vector<std::vector<double>>& upperlimit_distances, std::vector<int>& v, int V, int H, std::vector<int>& used_vertices, const vec_holenum& v_h, long long& counter) {
   constexpr int one_min = 1000 * 60;
   counter++;
   if (/*counter % 50 == 0*/ false) {
@@ -449,7 +450,7 @@ SSolutionPtr dfs_holes(SProblemPtr problem, SSolutionPtr solution, HashMap& hash
         //LOG(INFO) << "dfs_holes:: v is ";
         //for (auto e : v) LOG(INFO) << e;
         solution->vertices[x] = problem->hole_polygon[hole_x];
-        auto sol = dfs_holes(problem, solution, hash_map_decided, hole_distances, vertices_distances, upperlimit_distances, v, V, H, timer, used_vertices, v_h, counter);
+        auto sol = dfs_holes(problem, solution, hash_map_decided, hole_distances, vertices_distances, upperlimit_distances, v, V, H, used_vertices, v_h, counter);
         if (sol) return sol;
         solution->vertices[x] = problem->vertices[x];
       }
@@ -463,7 +464,8 @@ SSolutionPtr dfs_holes(SProblemPtr problem, SSolutionPtr solution, HashMap& hash
 
 
 #if 1
-bool full_research(SProblemPtr problem, SSolutionPtr& solution, Timer& timer) {
+bool full_research(SProblemPtr problem, SSolutionPtr& solution) {
+  LOG(INFO) << "CALLED FULL RESEARCH";
   int V = problem->vertices.size();
   int H = problem->hole_polygon.size();
   LOG(INFO) << "V,H is " << V << ' ' << H;
@@ -517,7 +519,7 @@ bool full_research(SProblemPtr problem, SSolutionPtr& solution, Timer& timer) {
 
   LOG(INFO) << "start decided is " << decided_points_initial.size();
   HashMap hash_map_decided = {};
-  auto sol = dfs_holes(problem, solution_initial, hash_map_decided, hole_distances, vertices_distances, upperlimit_distances, decided_points_initial, V, H, timer, used_vertices, v_h, counter);
+  auto sol = dfs_holes(problem, solution_initial, hash_map_decided, hole_distances, vertices_distances, upperlimit_distances, decided_points_initial, V, H, used_vertices, v_h, counter);
   if (sol) {
     solution = sol;
     LOG(INFO) << "hash_map_size is" << hash_map_decided.size();
@@ -575,11 +577,11 @@ public:
   SolverOutputs solve(const SolverArguments& args) override {
     SolverOutputs ret;
 
-    Timer timer;
+    //Timer timer;
     ret.solution = args.problem->create_solution();
     if (args.optional_initial_solution) ret.solution = args.optional_initial_solution;
-    full_research(args.problem, ret.solution, timer);
-
+    full_research(args.problem, ret.solution);
+    LOG(INFO) << "finish fullresearch";
     // dummy.
     //std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
@@ -589,3 +591,278 @@ public:
 
 REGISTER_SOLVER("FullResearch", FullResearch);
 #endif
+
+
+
+namespace SimpleMatchFullResearch {
+
+constexpr int kNumTrialsPerComponent = 100000;
+constexpr int kMinComponentSize = 10;
+constexpr int kMinDegree = 2;
+
+namespace bg = boost::geometry;
+using BoostPoint = bg::model::d2::point_xy<double>;
+using BoostPolygon = bg::model::polygon<BoostPoint>;
+using BoostLinestring = bg::model::linestring<BoostPoint>;
+
+template <typename T>
+BoostPoint ToBoostPoint(const T& point) {
+  const auto [x, y] = point;
+  return BoostPoint(x, y);
+}
+
+template <typename T>
+BoostPolygon ToBoostPolygon(const std::vector<T>& points) {
+  BoostPolygon polygon;
+  for (std::size_t i = 0; i <= points.size(); ++i) {
+    polygon.outer().push_back(ToBoostPoint(points[i % points.size()]));
+  }
+  if (bg::area(polygon) < 0.0) {
+    bg::reverse(polygon);
+  }
+  return polygon;
+}
+
+template <typename T, typename U>
+auto SquaredDistance(const T& vertex0, const U& vertex1) {
+  const auto [x0, y0] = vertex0;
+  const auto [x1, y1] = vertex1;
+  return (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1);
+}
+
+class Solver : public SolverBase {
+ public:
+  SolverOutputs solve(const SolverArguments& args) override {
+    counter_ = 0;
+    if (args.visualize) {
+      editor_ = std::make_shared<SVisualEditor>(args.problem, "SimpleMatchingSolver", "visualize");
+    }
+
+    hole_ = args.problem->hole_polygon;
+    vertices_ = args.problem->vertices;
+    edges_ = args.problem->edges;
+    epsilon_ = args.problem->epsilon;
+    const auto hole_polygon = ToBoostPolygon(hole_);
+
+    N_ = vertices_.size();
+    queued_.assign(N_, -1);
+    assigned_.assign(N_, -1);
+    adjacent_.assign(N_, {});
+    std::vector<std::vector<double>> distances(
+        N_, std::vector<double>(N_, std::numeric_limits<double>::infinity()));
+    for (int i = 0; i < N_; ++i) {
+      distances[i][i] = 0.0;
+    }
+    for (const auto& [a, b] : edges_) {
+      const auto squared_distance = SquaredDistance(vertices_[a], vertices_[b]);
+      const auto margin = epsilon_ * squared_distance / 1'000'000;
+      const auto min = squared_distance - margin;
+      const auto max = squared_distance + margin;
+      adjacent_[a].emplace_back(b, min, max);
+      adjacent_[b].emplace_back(a, min, max);
+      distances[a][b] = distances[b][a] = std::sqrt(squared_distance) * (1.0 + 1.0e-6 * epsilon_);
+    }
+    for (int k = 0; k < N_; ++k) {
+      for (int i = 0; i < N_; ++i) {
+        for (int j = 0; j < N_; ++j) {
+          distances[i][j] = std::min(distances[i][j], distances[i][k] + distances[k][j]);
+        }
+      }
+    }
+    vertex_max_distances_.resize(N_);
+    for (int i = 0; i < N_; ++i) {
+      vertex_max_distances_[i].resize(N_);
+      for (int j = 0; j < N_; ++j) {
+        vertex_max_distances_[i][j] = std::ceil(std::pow(distances[i][j], 2.0));
+      }
+    }
+
+    M_ = hole_.size();
+    hole_candidates_.clear();
+    hole_distances_.resize(M_);
+    hole_visibilities_.resize(M_);
+    for (int i = 0; i < M_; ++i) {
+      hole_candidates_.push_back(i);
+      hole_distances_[i].resize(M_);
+      hole_visibilities_[i].resize(M_);
+      for (int j = 0; j < M_; ++j) {
+        const BoostLinestring linestring{ToBoostPoint(hole_[i]), ToBoostPoint(hole_[j])};
+        hole_visibilities_[i][j] = bg::covered_by(linestring, hole_polygon);
+        hole_distances_[i][j] = SquaredDistance(hole_[i], hole_[j]);
+      }
+    }
+
+    last_updated_ = 0;
+    component_sizes_.assign(M_, M_);
+    best_sizes_.assign(M_, 0);
+    auto pose = vertices_;
+    for (int i = 0; i < N_; ++i) {
+      queued_[i] = N_;
+      vertex_candidates_.push_back(i);
+      assigned_counts_.assign(1, 0);
+      if (Search()) {
+        for (int j = 0; j < N_; ++j) {
+          if (assigned_[j] < 0) continue;
+          pose[j] = hole_[assigned_[j]];
+        }
+        break;
+      }
+      vertex_candidates_.pop_back();
+      queued_[i] = -1;
+    }
+
+        //Cleanup(0);
+    auto clean_candidates = Cleanup();
+    RevertClean(clean_candidates);
+
+    for (int i = 0; i < 2; i++) {
+      auto tmp_pose = pose;
+      if (i) for (auto e : clean_candidates) tmp_pose[e.first] = args.problem->vertices[e.first];
+      SSolutionPtr solution_simple = std::make_shared<SSolution>(tmp_pose);
+      auto solved = full_research(args.problem, solution_simple);
+      if (solved) return  SolverOutputs{ solution_simple };
+    }
+    return SolverOutputs{ std::make_shared<SSolution>(pose) };
+  }
+
+
+  std::vector<std::pair<int,int>> Cleanup(int max_clean = 1000) {
+    std::vector<std::pair<int,int>> ret = {};
+    if (!max_clean) return ret;
+    int counter = 0;
+    while (true) {
+      bool converged = true;
+      for (int i = 0; i < N_; ++i) {
+        if (assigned_[i] < 0) continue;
+        int degree = 0;
+        for (const auto& [next, min, max] : adjacent_[i]) {
+          degree += assigned_[next] >= 0;
+        }
+        if (degree < kMinDegree) {
+          ret.push_back(std::make_pair(i,assigned_[i]));
+          converged = false;
+          assigned_[i] = -1;
+          counter++;
+          if (counter >= max_clean) return ret;
+        }
+      }
+      if (converged) return ret;
+    }
+  }
+  void Clean(const std::vector<std::pair<int, int>>& rev) {
+    for (auto e : rev) assigned_[e.first] = -1;
+
+  }
+
+  void RevertClean(const std::vector<std::pair<int,int>>& rev) {
+    for (auto e : rev) assigned_[e.first] = e.second;
+  }
+
+  bool Search() {
+    if (hole_candidates_.empty()) return true;
+    if (++counter_ % 1000 == 0 && editor_) {
+      auto pose = vertices_;
+      std::vector<int> marked;
+      for (int j = 0; j < N_; ++j) {
+        if (assigned_[j] < 0) continue;
+        pose[j] = hole_[assigned_[j]];
+        marked.push_back(j);
+      }
+      editor_->set_pose(std::make_shared<SSolution>(pose));
+      editor_->set_marked_indices(marked);
+      editor_->set_persistent_custom_stat(fmt::format("assigned counts = {}", fmt::join(assigned_counts_, ", ")));
+      editor_->show(1);
+    }
+    const int component_index = assigned_counts_.size() - 1;
+    best_sizes_[component_index] = std::max(best_sizes_[component_index], assigned_counts_[component_index]);
+    if (counter_ > last_updated_ + kNumTrialsPerComponent) {
+      last_updated_ = counter_;
+      component_sizes_[component_index] = best_sizes_[component_index];
+      if (component_sizes_[component_index] < kMinComponentSize) return true;
+    }
+    for (int i = vertex_candidates_.size() - 1; i >= 0; --i) {
+      const int vertex = vertex_candidates_[i];
+      vertex_candidates_.erase(vertex_candidates_.begin() + i);
+      for (int j = hole_candidates_.size() - 1; j >= 0; --j) {
+        const int hole_vertex = hole_candidates_[j];
+        bool feasible = true;
+        for (const auto& [next, min, max] : adjacent_[vertex]) {
+          if (assigned_[next] < 0) continue;
+          const auto squared_distance = hole_distances_[assigned_[next]][hole_vertex];
+          const auto visible = hole_visibilities_[assigned_[next]][hole_vertex];
+          if (!visible || squared_distance < min || squared_distance > max) {
+            feasible = false;
+            break;
+          }
+        }
+        for (int other = 0; feasible && other < N_; ++other) {
+          if (assigned_[other] < 0) continue;
+          if (hole_distances_[assigned_[other]][hole_vertex] > vertex_max_distances_[other][vertex]) {
+            feasible = false;
+          }
+        }
+        if (feasible) {
+          hole_candidates_.erase(hole_candidates_.begin() + j);
+          assigned_[vertex] = hole_vertex;
+          ++assigned_counts_.back();
+          for (const auto& [next, min, max] : adjacent_[vertex]) {
+            if (queued_[next] >= 0) continue;
+            queued_[next] = vertex;
+            vertex_candidates_.push_back(next);
+          }
+          if (Search()) return true;
+          for (const auto& [next, min, max] : adjacent_[vertex]) {
+            if (queued_[next] != vertex) continue;
+            queued_[next] = -1;
+            vertex_candidates_.pop_back();
+          }
+          --assigned_counts_.back();
+          assigned_[vertex] = -1;
+          hole_candidates_.insert(hole_candidates_.begin() + j, hole_vertex);
+        }
+      }
+      vertex_candidates_.insert(vertex_candidates_.begin() + i, vertex);
+    }
+    if (assigned_counts_.back() >= component_sizes_[component_index]) {
+      if (assigned_counts_.size() == component_sizes_.size()) return true;
+      for (int i = 0; i < N_; ++i) {
+        if (queued_[i] >= 0) continue;
+        queued_[i] = N_;
+        vertex_candidates_.push_back(i);
+        assigned_counts_.push_back(0);
+        if (Search()) return true;
+        assigned_counts_.pop_back();
+        vertex_candidates_.pop_back();
+        queued_[i] = -1;
+      }
+    }
+    return false;
+  }
+
+ private:
+  std::vector<Point> hole_;
+  std::vector<Point> vertices_;
+  std::vector<Edge> edges_;
+  integer epsilon_;
+
+  int N_;
+  int M_;
+  std::vector<int> queued_;
+  std::vector<int> assigned_;
+  std::vector<int> vertex_candidates_;
+  std::vector<int> hole_candidates_;
+  std::vector<std::vector<std::tuple<int, integer, integer>>> adjacent_;
+  std::vector<std::vector<integer>> hole_distances_;
+  std::vector<std::vector<bool>> hole_visibilities_;
+  std::vector<std::vector<integer>> vertex_max_distances_;
+  std::vector<int> assigned_counts_;
+  std::vector<int> component_sizes_;
+  std::vector<int> best_sizes_;
+  std::size_t last_updated_;
+  std::size_t counter_;
+  SVisualEditorPtr editor_;
+};
+
+}
+REGISTER_SOLVER("SimpleMatchFullResearch", SimpleMatchFullResearch::Solver);
+
