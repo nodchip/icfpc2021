fa5b99dba01192b2594b1c6e7210bb9b7e0f0041
Hisayori Noda <nodchip@gmail.com>
2021-07-12 17:14:48 +0900

    Changed to specify the first and the last build number to be stopped. Fixed an option type. Fixed the host name.

--- scripts/execute_jenkins_optuna_jobs.py
+++ scripts/execute_jenkins_optuna_jobs.py
@@ -21,17 +21,17 @@ def main():
     parser.add_argument('token', help='Jenkins API Token.')
     parser.add_argument('--stop', action='store_true',
                         help='Stop the current jobs.')
+    parser.add_argument('--first_build_number', type=int,
+                        help='First build number to be stopped.')
+    parser.add_argument('--last_build_number', type=int,
+                        help='Last build number to be stopped.')
     parser.add_argument('--start', action='store_true', help='Start jobs.')
-    parser.add_argument('--solver_name', type=int, help='Solver name')
+    parser.add_argument('--solver_name', type=str, help='Solver name')
     args = parser.parse_args()
 
     if args.stop:
-        # jenkins — Jenkinsから、現在実行中のジョブのリストをJSONで取得するにはどうすればよいですか？ https://www.it-swarm-ja.com/ja/jenkins/jenkins%E3%81%8B%E3%82%89%E3%80%81%E7%8F%BE%E5%9C%A8%E5%AE%9F%E8%A1%8C%E4%B8%AD%E3%81%AE%E3%82%B8%E3%83%A7%E3%83%96%E3%81%AE%E3%83%AA%E3%82%B9%E3%83%88%E3%82%92json%E3%81%A7%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B%E3%81%AB%E3%81%AF%E3%81%A9%E3%81%86%E3%81%99%E3%82%8C%E3%81%B0%E3%82%88%E3%81%84%E3%81%A7%E3%81%99%E3%81%8B%EF%BC%9F/1071072010/
-        url = f'http://hnoda-dt2:8080/job/ICFPC2021.optimize_parameters.2021-07-11/api/json'
-        with urllib.request.urlopen(url) as f:
-            jenkins = json.loads(f.read())
-        for build in jenkins['builds']:
-            url = f'{build["url"]}stop'
+        for build_number in range(args.first_build_number, args.last_build_number + 1):
+            url = f'http://jenkins:8080/job/ICFPC2021.optimize_parameters.2021-07-11/{build_number}/stop'
             print(url)
             response = requests.post(url, auth=('hnoda', args.token))
             print(response.status_code)
@@ -41,11 +41,16 @@ def main():
         with open(REPO_DIR / 'www' / 'contest_infos.tsv', encoding='utf-8', newline='') as f:
             for row in csv.reader(f, delimiter='\t'):
                 problem_id = row[0]
+                if problem_id == 'problem':
+                    continue
                 dislikes = row[1]
-                if dislikes == 0:
+                # 満点が求まっている問題についてはソルバーを実行しない。
+                # ただし、dislikesが空欄の問題は、有効な出力が得られていないので、
+                # ソルバーを実行する。
+                if dislikes == '0':
                     continue
                 # Parameterized Build - Jenkins - Jenkins Wiki https://wiki.jenkins.io/display/JENKINS/Parameterized+Build
-                url = f'http://hnoda-dt2:8080/job/ICFPC2021.optimize_parameters.2021-07-11/buildWithParameters?problem_id={problem_id}&solver_name={args.solver_name}'
+                url = f'http://jenkins:8080/job/ICFPC2021.optimize_parameters.2021-07-11/buildWithParameters?problem_id={problem_id}&solver_name={args.solver_name}'
                 print(url)
                 requests.post(url, auth=('hnoda', args.token))
 
